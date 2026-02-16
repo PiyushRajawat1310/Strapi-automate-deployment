6c10b77f156e07dc29d7d5d21b16c1b66cc6f5a6
PiyushRajawat1310 <223863796+PiyushRajawat1310@users.noreply.github.com>
2026-02-16 18:18:13 +0530

    Update main.tf

--- my-strapi/terraform/main.tf
+++ my-strapi/terraform/main.tf
@@ -1,40 +1,54 @@
-provider "aws" {
-  region = var.aws_region
-}
+resource "aws_security_group" "strapi_sg" {
+  name        = "strapi-sg-final"
+  description = "Allow SSH and Strapi"
 
-resource "aws_instance" "strapi" {
-  ami           = var.ami
-  instance_type = "t3.micro"
-  key_name      = var.key_name
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
-  user_data = <<-EOF
-#!/bin/bash
-set -e
+  ingress {
+    from_port   = 1337
+    to_port     = 1337
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
-apt-get update -y
-apt-get install -y docker.io
-
-systemctl enable docker
-systemctl start docker
-
-sleep 20
-
-docker pull ${var.image}
-
-docker rm -f strapi || true
-
-docker run -d \
-  --restart always \
-  --name strapi \
-  -e HOST=0.0.0.0 \
-  -e PORT=1337 \
-  -p 1337:1337 \
-  ${var.image}
-EOF
-
-
-  tags = {
-    Name = "strapi-server"
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 }
 
+resource "aws_instance" "strapi_server" {
+  ami           = "ami-0f5ee92e2d63afc18" # Ubuntu 22.04 ap-south-1
+  instance_type = "t3.micro"
+  key_name      = var.key_name
+
+  vpc_security_group_ids = [aws_security_group.strapi_sg.id]
+
+  user_data = <<-EOF
+#!/bin/bash
+apt-get update -y
+apt-get install -y docker.io
+systemctl start docker
+systemctl enable docker
+docker pull ${var.docker_image}
+docker rm -f strapi || true
+docker run -d \
+  -e HOST=0.0.0.0 \
+  -e PORT=1337 \
+  -p 1337:1337 \
+  --restart always \
+  --name strapi \
+  ${var.docker_image}
+EOF
+
+  tags = {
+    Name = "Strapi-Server"
+  }
+}
