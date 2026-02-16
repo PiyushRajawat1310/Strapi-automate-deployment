7da58aaf64096d273174c5495d1e6ee5f6179bde
PiyushRajawat1310 <223863796+PiyushRajawat1310@users.noreply.github.com>
2026-02-16 18:19:04 +0530

    Rename output to ec2_public_ip and add strapi_url

--- my-strapi/terraform/outputs.tf
+++ my-strapi/terraform/outputs.tf
@@ -1,4 +1,7 @@
-output "public_ip" {
-  value = aws_instance.strapi.public_ip
+output "ec2_public_ip" {
+  value = aws_instance.strapi_server.public_ip
 }
 
+output "strapi_url" {
+  value = "http://${aws_instance.strapi_server.public_ip}:1337"
+}
