89c55855edf884eb4fd778e390fa089412d32b8d
piyushrajawat1310 <piyushrajawat1310@gmail.com>
2026-02-16 11:20:51 +0000

    edit ci.yml

--- my-strapi/terraform/main.tf
+++ my-strapi/terraform/main.tf
@@ -8,12 +8,30 @@ resource "aws_instance" "strapi" {
   key_name      = var.key_name
 
   user_data = <<-EOF
-    #!/bin/bash
-    apt update -y
-    apt install docker.io -y
-    systemctl start docker
-    docker run -d -p 1337:1337 ${var.image}
-  EOF
+#!/bin/bash
+set -e
+
+apt-get update -y
+apt-get install -y docker.io
+
+systemctl enable docker
+systemctl start docker
+
+sleep 20
+
+docker pull ${var.image}
+
+docker rm -f strapi || true
+
+docker run -d \
+  --restart always \
+  --name strapi \
+  -e HOST=0.0.0.0 \
+  -e PORT=1337 \
+  -p 1337:1337 \
+  ${var.image}
+EOF
+
 
   tags = {
     Name = "strapi-server"
--- my-strapi/terraform/variables.tf
+++ my-strapi/terraform/variables.tf
@@ -1,5 +1,16 @@
-variable "aws_region" {}
-variable "ami" {}
-variable "key_name" {}
-variable "image" {}
+variable "aws_region" {
+  default = "ap-south-1"
+}
+
+variable "ami" {
+  default = "ami-0f5ee92e2d63afc18" # Ubuntu 22.04 Mumbai
+}
+
+variable "key_name" {
+  default = "strapi-key"
+}
+
+variable "image" {
+  description = "Docker image"
+}
 
