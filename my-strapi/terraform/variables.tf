1954465e50d9516d830545d522840a76634881ca
PiyushRajawat1310 <223863796+PiyushRajawat1310@users.noreply.github.com>
2026-02-16 18:19:44 +0530

    Enhance variable definitions in variables.tf
    Updated variable definitions with descriptions and added docker_image variable.

--- my-strapi/terraform/variables.tf
+++ my-strapi/terraform/variables.tf
@@ -1,16 +1,17 @@
 variable "aws_region" {
-  default = "ap-south-1"
-}
-
-variable "ami" {
-  default = "ami-0f5ee92e2d63afc18" # Ubuntu 22.04 Mumbai
+  description = "AWS region"
+  type        = string
+  default     = "ap-south-1"
 }
 
 variable "key_name" {
-  default = "strapi-key"
+  description = "EC2 key pair name"
+  type        = string
+  default     = "strapi-key"
 }
 
-variable "image" {
-  description = "Docker image"
+variable "docker_image" {
+  description = "Docker image for Strapi"
+  type        = string
+  default     = "YOUR_DOCKERHUB_USERNAME/strapi:latest"
 }
-
